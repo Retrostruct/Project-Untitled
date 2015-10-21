f331a03e21eb5f390ac92d93b5ae84c8841a5c86
igefjord <simon.igefjord@gmail.com>
2015-10-21 11:58:36 +0200

    Test

--- Code/Engine/core/src/com/retrostruct/epsilon/Game1.java
+++ Code/Engine/core/src/com/retrostruct/epsilon/Game1.java
@@ -18,10 +18,13 @@ public class Game1 extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+        if(Gdx.input.isTouched()) {
+            batch.draw(img, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+        }
 		batch.end();
 	}
 }
