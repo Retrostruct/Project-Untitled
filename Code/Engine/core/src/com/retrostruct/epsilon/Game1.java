b17a121e8bc2f8779f55b3738f3b3c56576462b5
OstGeneralen <kasper.esbjornsson@gmail.com>
2015-10-22 11:13:11 +0200

    Added some pointless code
    Added some pointless code for testing och physical device

--- Code/Engine/core/src/com/retrostruct/epsilon/Game1.java
+++ Code/Engine/core/src/com/retrostruct/epsilon/Game1.java
@@ -5,26 +5,57 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class Game1 extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Vector2 goTo;
+	Vector2 currentPosition;
+	float speed;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		currentPosition = new Vector2(0,0);
+		goTo = new Vector2(0,0);
+		speed = 5;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+
+		if(Gdx.input.isTouched()){
+			goTo.x = Gdx.input.getX();
+			goTo.y = Gdx.input.getY();
+		}
+
+		if(currentPosition != goTo)
+		{
+			if(goTo.x > currentPosition.x){
+				currentPosition.x+=speed;
+			}
+			else if(goTo.x < currentPosition.x){
+				currentPosition.x-=speed;
+			}
+
+			if(goTo.y > currentPosition.y)
+			{
+				currentPosition.y+=speed;
+			}
+			else if(goTo.y < currentPosition.y){
+				currentPosition.y-=speed;
+			}
+		}
+
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		batch.begin();
-        if(Gdx.input.isTouched()) {
-            batch.draw(img, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
-        }
+
+		batch.draw(img, currentPosition.x, Gdx.graphics.getHeight() - currentPosition.y);
+
 		batch.end();
 	}
 }
