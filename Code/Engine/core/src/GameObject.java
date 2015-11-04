3fb0446300df51df1e8b286a187a4222fcaf8d04
OstGeneralen <kasper.esbjornsson@gmail.com>
2015-11-04 14:00:13 +0100

    Added basic derrivatable code

--- Code/Engine/core/src/GameObject.java
+++ Code/Engine/core/src/GameObject.java
@@ -1,5 +1,22 @@
 /**
  * Created by kasper.esbjornsson on 11/4/2015.
  */
-public class GameObject {
+public class GameObject extends Interactable {
+
+    public GameObject(){
+
+    }
+
+    @Override
+    public void onInteract(Interactions interaction) {
+
+        if(super.interactionPossible(interaction)){
+
+
+
+        }
+
+    }
+
+
 }
--- Code/Engine/core/src/Interactable.java
+++ Code/Engine/core/src/Interactable.java
@@ -1,5 +1,40 @@
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.List;
+
 /**
  * Created by kasper.esbjornsson on 11/4/2015.
  */
-public class Interactable {
+public abstract class Interactable {
+
+    protected List<Interactions> possibleInteractions;
+    protected Vector2 position;
+    protected Texture texture;
+
+    public Interactable(){
+
+    }
+
+    public void onLoad(){
+
+    }
+
+    public List<Interactions> getPossibleInteractions(){
+     return possibleInteractions;
+    }
+
+    public void onInteract(Interactions interaction){
+
+    }
+
+    protected boolean interactionPossible(Interactions interaction){
+        for(Interactions i: possibleInteractions) {
+            if(interaction == i){
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
