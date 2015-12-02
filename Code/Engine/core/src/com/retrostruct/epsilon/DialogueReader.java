76a61b761a3392c433f05fbdc4d191caf29b95c4
OstGeneralen <kasper.esbjornsson@gmail.com>
2015-12-02 11:29:04 +0100

    Added more code to dialogue reader

--- Code/Engine/core/src/com/retrostruct/epsilon/DialogueReader.java
+++ Code/Engine/core/src/com/retrostruct/epsilon/DialogueReader.java
@@ -1,7 +1,23 @@
 package com.retrostruct.epsilon;
 
+import java.io.File;
+
+import javax.sql.rowset.spi.XmlReader;
+
+import jdk.internal.org.xml.sax.XMLReader;
+
 /**
  * Created by kasper.esbjornsson on 12/2/2015.
  */
 public class DialogueReader {
+
+    File file;
+
+    public DialogueReader(String dialogueFile){
+        file = new File("Assets/" + dialogueFile + ".xml");
+
+        
+    }
+
+
 }
