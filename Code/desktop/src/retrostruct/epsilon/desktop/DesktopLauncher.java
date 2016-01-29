114e89def6f849b4f1bea039102cb19feeb001b4
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2016-01-29 09:05:41 +0100

    Changes to .gradle files and launcher .java

--- Code/desktop/src/retrostruct/epsilon/desktop/DesktopLauncher.java
+++ Code/desktop/src/retrostruct/epsilon/desktop/DesktopLauncher.java
@@ -1,10 +1,5 @@
 package retrostruct.epsilon.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
-import retrostruct.epsilon.GdxGame;
-
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
--- Code/html/src/retrostruct/epsilon/client/HtmlLauncher.java
+++ Code/html/src/retrostruct/epsilon/client/HtmlLauncher.java
@@ -1,10 +1,5 @@
 package retrostruct.epsilon.client;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.backends.gwt.GwtApplication;
-import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import retrostruct.epsilon.GdxGame;
-
 public class HtmlLauncher extends GwtApplication {
 
         @Override
